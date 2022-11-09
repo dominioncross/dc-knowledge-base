c236bcf0670498d23e9219025b0db4e54b3eebc3
Ben Petro <benpetro@gmail.com>
2022-11-09 14:47:10 +1000

    session_name fix

--- lib/knowledge-base/configuration.rb
+++ lib/knowledge-base/configuration.rb
@@ -1,11 +1,10 @@
 module KnowledgeBase
   class Configuration
 
-    cattr_accessor :scope_class, :mongoid_session_name, :messages_require_approval
+    cattr_accessor :scope_class, :messages_require_approval
 
     def self.reset
       self.scope_class                    = nil
-      self.mongoid_session_name           = :forklift
       self.messages_require_approval      = false
     end
 
